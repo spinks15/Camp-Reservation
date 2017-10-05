f352e0e3bc069be361bcacae1573423e0ce7ebdb
Brian Lauvray <brian@techelevator.com>
2017-10-05 08:50:16 -0400

    added m2 capstone

--- m2-w6d1-dao-exercises-pair/ProjectDB/DAL/DepartmentSqlDAL.cs
+++ m2-w6d1-dao-exercises-pair/ProjectDB/DAL/DepartmentSqlDAL.cs
@@ -1,6 +1,7 @@
 ﻿using ProjectDB.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace ProjectDB.DAL
 {
     public class DepartmentSqlDAL
     {
+
+        private const string getDepartmentsSql = "select * from department";
         private string connectionString;
 
         // Single Parameter Constructor
@@ -19,7 +22,29 @@ namespace ProjectDB.DAL
 
         public List<Department> GetDepartments()
         {
-            throw new NotImplementedException();
+            List<Department> departments = new List<Department>();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand command = new SqlCommand(getDepartmentsSql, conn);
+                    SqlDataReader results = command.ExecuteReader();
+
+                    while(results.Read())
+                    {
+                        departments.Add(CreateDepartmentFromRow(results));
+                    }
+
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw;
+            }
+
+            return departments;
         }
 
         public bool CreateDepartment(Department newDepartment)
@@ -30,6 +55,14 @@ namespace ProjectDB.DAL
         public bool UpdateDepartment(Department updatedDepartment)
         {
             throw new NotImplementedException();
+        }
+
+        private Department CreateDepartmentFromRow(SqlDataReader results)
+        {
+            Department dept = new Department();
+            dept.Id = Convert.ToInt32(results["department_id"]);
+            dept.Name = Convert.ToString(results["name"]);
+            return dept;
         }
 
     }
--- m2-w6d1-dao-exercises-pair/ProjectDB/ProjectCLI.cs
+++ m2-w6d1-dao-exercises-pair/ProjectDB/ProjectCLI.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace ProjectDB
 {
@@ -21,7 +22,8 @@ namespace ProjectDB
         const string Command_AssignEmployeeToProject = "9";
         const string Command_RemoveEmployeeFromProject = "10";
         const string Command_Quit = "q";
-        const string DatabaseConnection = "";
+        private string DatabaseConnection = 
+            ConfigurationManager.ConnectionStrings["ProjectsConnection"].ConnectionString;
 
         public void RunCLI()
         {
